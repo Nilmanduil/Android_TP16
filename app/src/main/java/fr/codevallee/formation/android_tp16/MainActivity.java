64ac92e2c1f6fc7473acb40c7b8c741a9fadb2b0
Thibault Goudouneix <nilmanduil@gmail.com>
2017-10-18 10:16:52 +0200

    First commit

---
+++ app/src/main/java/fr/codevallee/formation/android_tp16/MainActivity.java
@@ -0,0 +1,25 @@
+package fr.codevallee.formation.android_tp16;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) { // Si layout pour écran non large alors :
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            ListeUtilisateurFragment firstFragment = new ListeUtilisateurFragment();
+            firstFragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+        }
+    }
+}
